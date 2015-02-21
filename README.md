ad83a9417d1393db0109cead0e10b3f555f04fdd
Juan Rosero <pacho-uno@hotmail.com>
2015-02-21 11:51:38 -0500

    First Commit for Pilots

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pilots
+Copyright 2015 Juan Rosero
---
+++ js/pilots.js
@@ -0,0 +1,24 @@
+
+
+function handleProfile(e) 
+{
+	
+	var elem = document.getElementById("mutltiplayer");
+	elem.style.display = "none";
+	
+	elem = document.getElementById("profile");
+	elem.style.display = "block";
+	
+}
+
+    function handleMultiplayer(e) 
+{
+	
+	var elem = document.getElementById("mutltiplayer");
+	elem.style.display = "block";
+	
+	elem = document.getElementById("profile");
+	elem.style.display = "none";
+	
+
+}
